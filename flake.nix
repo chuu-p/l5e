1bbe5f5d382246d647a679b4a932ec4105876a56
chuu-p <chuu801@pm.me>
2025-11-20 00:04:13 +0100

    🛠️ (flake.nix): update nix flake to support multiple systems and disable tests in CI/CD workflow

--- flake.nix
+++ flake.nix
@@ -5,27 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        # dev dependencies
-        prettier
-        docker
-        natscli
-        nats-server
-        nats-top
-        openapi-generator-cli
-        nodejs
-        pnpm
-        tailwindcss
-      ];
+  outputs = { self, nixpkgs, ... }:
+    let
+      # Let Nix pick the current host system
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              prettier
+              docker
+              natscli
+              nats-server
+              nats-top
+              openapi-generator-cli
+              nodejs
+              pnpm
+              tailwindcss
+            ];
+          };
+        }
+      );
     };
-  };
 }
+
