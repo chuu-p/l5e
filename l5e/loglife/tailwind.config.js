e640fcc42512533020d3f0ade1df1e5da456f258
chuu-p <chuu801@pm.me>
2026-02-17 02:12:42 +0100

    feat: add initial configuration for nativewind and tailwind

---
+++ l5e/loglife/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // We include your specific folder structure here
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        void: "#050505", // Deep black
+        neon: "#CCFF00", // Edgy yellow
+        zinc: {
+          900: "#18181b",
+          800: "#27272a",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
